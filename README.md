1fc2e0dfe1cc129128a3e85aa7ff5fcacb83bb48
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2023-01-07 12:38:52 +0100

    Adding mod files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Rimworld-ShowGenderAndAgeInCaravanFormDialog
- Show gender and age of pawns in caravan formation dialog
+[![Version](https://img.shields.io/badge/Rimworld-1.4-green.svg)](http://rimworldgame.com/)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/angelolocritani/Rimworld-ShowGenderAgeCaravanFormDialog)](https://github.com/angelolocritani/Rimworld-ShowGenderAgeCaravanFormDialog/releases/latest)
+# Show gender/age in caravan form dialog
+
+![Rimworld-ShowGenderAndAgeInCaravanFormDialogPreview](https://i.imgur.com/xSZSreH.png)
+
+A mod that adds gender/age in caravan form dialog, like when trading
+
+
+---
+
+[![Steam download](https://img.shields.io/steam/downloads/2913588675?logo=steam)](https://steamcommunity.com/sharedfiles/filedetails/?id=2913588675)
+
---
+++ Source/ShowGenderAndAgeInCaravanFormDialog/ShowGenderAgeCaravanFormDialog.cs
@@ -0,0 +1,49 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+using System;
+
+namespace ShowGenderAndAgeInCaravanFormDialog
+{
+
+
+    [StaticConstructorOnStartup]
+    public static class ShowGenderAgeCaravanFormDialogMod
+    {
+        public static Harmony harmonyInstance;
+
+        static ShowGenderAgeCaravanFormDialogMod()
+        {
+            harmonyInstance = new Harmony("arl85.CorpseInfo");
+            harmonyInstance.PatchAll();
+        }
+
+
+        [HarmonyPatch(typeof(TransferableOneWay), "Label", MethodType.Getter)]
+        public static class TransferableOneWay_Label
+        {
+            static void Postfix(ref string __result, TransferableOneWay __instance)
+            {
+
+                if (__instance.AnyThing is Pawn pawn)
+                {
+                    if (pawn.Name != null && !pawn.Name.Numerical && !pawn.RaceProps.Humanlike)
+                    {
+                        __result += ", " + pawn.def.label;
+                    }
+                 
+                    __result += " (" + pawn.GetGenderLabel() + ", " + Mathf.FloorToInt(pawn.ageTracker.AgeBiologicalYearsFloat) + ")";
+
+                }
+            }
+
+
+        }
+
+    }
+
+
+}
+
